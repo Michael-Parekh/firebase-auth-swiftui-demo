911acb802c1d488c2e049d5a0ca3c3bf3f80c5c8
Michael Parekh <mparekh4@illinois.edu>
2024-01-22 15:48:41 -0600

    init project

---
+++ FirebaseAuthSwiftUIDemo/FirebaseAuthSwiftUIDemo/App/FirebaseAuthSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthSwiftUIDemoApp.swift
+//  FirebaseAuthSwiftUIDemo
+//
+//  Created by Michael Parekh on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
