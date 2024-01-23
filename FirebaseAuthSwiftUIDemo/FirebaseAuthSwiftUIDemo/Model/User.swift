b4a47ab190b8338126adfd35d0a40e8b8da55bc7
Michael Parekh <mparekh4@illinois.edu>
2024-01-23 00:00:58 -0600

    [backend] create User data model + mock data

---
+++ FirebaseAuthSwiftUIDemo/FirebaseAuthSwiftUIDemo/Model/User.swift
@@ -0,0 +1,29 @@
+//
+//  User.swift
+//  FirebaseAuthSwiftUIDemo
+//
+//  Created by Michael Parekh on 1/22/24.
+//
+
+import Foundation
+
+// For any app that requires authentication, we need to create a data model representing a User object.
+struct User: Identifiable, Codable {
+    let id: String
+    let fullname: String
+    let email: String
+    
+    // Convert the 'fullname' property into initials for the ProfileView.
+    var initials: String {
+        let formatter = PersonNameComponentsFormatter()
+        if let components = formatter.personNameComponents(from: fullname) {
+            formatter.style = .abbreviated
+            return formatter.string(from: components)
+        }
+        return ""
+    }
+}
+
+extension User {
+    static var MOCK_USER = User(id: NSUUID().uuidString, fullname: "Michael Parekh", email: "test@gmail.com")
+}
