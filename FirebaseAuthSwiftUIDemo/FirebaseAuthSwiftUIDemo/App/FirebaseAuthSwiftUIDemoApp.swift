1af3139dde200ede2f89e315aca0a771ffcad9d1
Michael Parekh <mparekh4@illinois.edu>
2024-01-23 17:42:32 -0600

    [firebase][backend] implement the createUser method for AuthViewModel

--- FirebaseAuthSwiftUIDemo/FirebaseAuthSwiftUIDemo/App/FirebaseAuthSwiftUIDemoApp.swift
+++ FirebaseAuthSwiftUIDemo/FirebaseAuthSwiftUIDemo/App/FirebaseAuthSwiftUIDemoApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirebaseAuthSwiftUIDemoApp: App {
@@ -13,6 +14,11 @@ struct FirebaseAuthSwiftUIDemoApp: App {
     // Use an EnvironmentObject to represent AuthViewModel instead of instantiating multiple StateObjects.
     // EnvironmentObjects get initialized in one place and can be used across multiple surfaces in the app (because we need to access AuthViewModel in LoginView, RegistrationView, ProfileView, and ContentView).
     @StateObject var viewModel = AuthViewModel()
+    
+    // We need to configure Firebase right when the app launches. 
+    init() {
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
--- FirebaseAuthSwiftUIDemo/FirebaseAuthSwiftUIDemo/Core/Authentication/ViewModel/AuthViewModel.swift
+++ FirebaseAuthSwiftUIDemo/FirebaseAuthSwiftUIDemo/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -7,16 +7,22 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 // Responsible for functionality associated with authenticating users (making the network calls) + updating the UI as needed.
 // Conforms to the ObservableObject protocol so that our view will be able to observe changes on AuthViewModel.
+
+// We need to publish all UI updates back on the main thread (by default, all of the asynchronous networking events happens on background threads).
+@MainActor
 class AuthViewModel: ObservableObject {
     // Tells us whether there is a user currently logged in (so that the app knows whether to route us to the Login flow or Profile flow).
+    // Because it's a Published property, it will send a notification to ContentView to reconfigure when the value is changed.
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: User?
     
     init() {
-        
+        // When the AuthViewModel initializes, it will check if there is already a logged in user.
+        self.userSession = Auth.auth().currentUser
     }
     
     // `async throws` indicates that the asynchronous function can also throw errors.
@@ -25,7 +31,19 @@ class AuthViewModel: ObservableObject {
     }
     
     func createUser(withEmail email: String, password: String, fullname: String) async throws {
-        print("Create user..")
+        do {
+            // 'try await' eliminates the need for us to use completion blocks and handlers.
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            
+            // Note that our User object has additional properties like email address - the Firebase user object is just the generic user object that Firebase creates on the backend.
+            let user = User(id: result.user.uid, fullname: fullname, email: email)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            // Store the encoded User object in Firestore.
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+        } catch {
+            print("DEBUG: Failed to create user with error \(error.localizedDescription)")
+        }
     }
     
     func signOut() {
