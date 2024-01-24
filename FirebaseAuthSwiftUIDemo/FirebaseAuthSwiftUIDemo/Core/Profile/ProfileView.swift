db799289012266bac990fdd4433d987a5c5b4bf7
Michael Parekh <mparekh4@illinois.edu>
2024-01-24 14:35:38 -0600

    [profile][ui] display User obj details from firebase

--- FirebaseAuthSwiftUIDemo/FirebaseAuthSwiftUIDemo/Core/Profile/ProfileView.swift
+++ FirebaseAuthSwiftUIDemo/FirebaseAuthSwiftUIDemo/Core/Profile/ProfileView.swift
@@ -8,56 +8,62 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @EnvironmentObject var viewModel: AuthViewModel
+    
     var body: some View {
-        List {
-            // MARK: User Info View
-            Section {
-                HStack {
-                    Text(User.MOCK_USER.initials)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(width: 72, height: 72)
-                        .background(Color(.systemGray3))
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(User.MOCK_USER.fullname)
-                            .font(.subheadline)
+        // If there is a User currently logged in, display their details. 
+        if let user = viewModel.currentUser {
+            List {
+                // MARK: User Info View
+                Section {
+                    HStack {
+                        Text(user.initials)
+                            .font(.title)
                             .fontWeight(.semibold)
-                            .padding(.top, 4)
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
+                            .foregroundColor(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray3))
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(user.fullname)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
+                // MARK: General View
+                Section("General") {
+                    HStack {
+                        SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+                        
+                        Spacer()
+                        
+                        Text("1.0.0")
+                            .font(.subheadline)
                             .foregroundColor(.gray)
                     }
                 }
-            }
-            
-            // MARK: General View
-            Section("General") {
-                HStack {
-                    SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
-                    
-                    Spacer()
-                    
-                    Text("1.0.0")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-            }
-            
-            // MARK: Account View
-            Section("Account") {
-                Button {
-                    print("Sign out...")
-                } label: {
-                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
-                }
                 
-                Button {
-                    print("Delete account...")
-                } label: {
-                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
+                // MARK: Account View
+                Section("Account") {
+                    Button {
+                        print("Sign out...")
+                    } label: {
+                        SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
+                    }
+                    
+                    Button {
+                        print("Delete account...")
+                    } label: {
+                        SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
+                    }
                 }
             }
         }
